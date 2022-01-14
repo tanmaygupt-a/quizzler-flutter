8b5c04f601f1bbde9da6e32a438eee26577a8a5e
tanmaygupta <tanmaygupta.dev@gmail.com>
2022-01-15 02:28:56 +0530

    Comment

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,13 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> icons = [
+    Icon(Icons.check, color: Colors.green),
+    Icon(Icons.close, color: Colors.red),
+    Icon(Icons.close, color: Colors.red),
+    Icon(Icons.close, color: Colors.red)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -50,9 +57,10 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.green,
+              ),
               child: Text(
                 'True',
                 style: TextStyle(
@@ -62,6 +70,9 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                setState(() {
+                  icons.add(new Icon(Icons.check, color: Colors.green));
+                });
               },
             ),
           ),
@@ -69,8 +80,8 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
+            child: TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.red),
               child: Text(
                 'False',
                 style: TextStyle(
@@ -79,12 +90,16 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(() {
+                  icons.removeLast();
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: icons,
+        )
       ],
     );
   }
